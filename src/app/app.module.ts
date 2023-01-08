07848c3b77963b4c26a7570ec7878a39c798d702
chetanjogi1@gmail.com <chetanjogi1@gmail.com>
2023-01-08 23:20:01 +0530

    Depedent Dropdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './pages/users/users.component';
-
+import { EmployeeComponent } from './pages/employee/employee.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent
+    UsersComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+
+  clientId: string = '';
+  siteId: string = '';
+  buildingId: string = '';
+  floorId: string = '';
+  clients: any []= [];
+  sites: any []= [];
+  buildings: any []= [];
+  floors: any []= [];
+  constructor(private http: HttpClient) {
+
+  }
+  ngOnInit() {
+    this.loadClients();
+  }
+
+  loadClients() {
+    this.http.get('http://onlinetestapi.gerasim.in/api/GetValet/GetAllClients').subscribe((result: any)=> {
+      this.clients = result.data;
+    })
+  }
+  getSitesByClinetId() {
+    this.http.get('http://onlinetestapi.gerasim.in/api/GetValet/GetAllSites?clientid='+this.clientId).subscribe((result: any)=> {
+      this.sites = result.data;
+    })
+  }
+  getBuildingBySiteId() {
+    this.http.get('http://onlinetestapi.gerasim.in/api/GetValet/GetBuildingBySiteId?id='+this.siteId).subscribe((result: any)=> {
+      this.buildings = result.data;
+    })
+  }
+  getFloorByBuilidngId() {
+    this.http.get('http://onlinetestapi.gerasim.in/api/GetValet/GetFloorsByBuildingId?id='+this.buildingId).subscribe((result: any)=> {
+      this.floors = result.data;
+    })
+  }
+
+
 
 }
