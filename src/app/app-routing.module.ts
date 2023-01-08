229a1ac89d943220fc4fb040187c652a907feb9b
chetanjogi1@gmail.com <chetanjogi1@gmail.com>
2023-01-08 21:38:53 +0530

    Added Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersComponent } from './pages/users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'user',
+    component: UsersComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
