111def466cba07f3539369d165fe3f17f79c7e70
chetanjogi1@gmail.com <chetanjogi1@gmail.com>
2023-01-13 00:32:03 +0530

    Stepper

--- src/app/pages/employee/employee.component.ts
+++ src/app/pages/employee/employee.component.ts
@@ -1,34 +1,54 @@
-import { Component, OnInit,ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { MyTableComponent } from 'src/app/reusable/my-table/my-table.component';
 
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
-  styleUrls: ['./employee.component.css']
+  styleUrls: ['./employee.component.css'],
 })
 export class EmployeeComponent {
-
-
-  @ViewChild (MyTableComponent) myTable: MyTableComponent =
-  new MyTableComponent();
-  users:any [] = [];
-  constructor(private http: HttpClient) {
-
-  }
+  activeTab: string = 'Basic';
+  currentSelectedRowId: number = 0;
+  users: any[] = [];
+  steps: any = [
+    {
+      step: 'Basic',
+      isComplete: true,
+      isActive: false
+    },
+    {
+      step: 'Contact',
+      isComplete: true,
+      isActive: false
+    },
+    {
+      step: 'Family',
+      isComplete: false,
+      isActive: true
+    },
+  ];
+  constructor(private http: HttpClient) {}
   ngOnInit() {
     this.loadUser();
-    setTimeout(() => {
-      debugger;
-      const data = this.myTable.columnArray;
-    }, 2000);
+  }
+  setStep(step: any) {
+    this.activeTab = step.step;
+  }
+  changeTab(taName: string) {
+    this.activeTab = taName;
+  }
+  onSelect(id: number) {
+    this.currentSelectedRowId = id;
   }
 
   loadUser() {
-    this.http.get('https://jsonplaceholder.typicode.com/users').subscribe((result: any)=> {
-      this.users = result;
-      debugger;
-    })
+    this.http
+      .get('https://jsonplaceholder.typicode.com/users')
+      .subscribe((result: any) => {
+        this.users = result;
+        debugger;
+      });
   }
   EditUser(record: any) {
     debugger;
