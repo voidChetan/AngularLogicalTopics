aa875b0d5909318faad941fcc2190e8ed115e50f
codefirst2022 <codefirst2022@gmail.com>
2024-03-09 17:11:12 +0530

    validation

--- src/app/pages/content-projection/content-projection.component.ts
+++ src/app/pages/content-projection/content-projection.component.ts
@@ -1,3 +1,4 @@
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class ContentProjectionComponent {
 
+  obj: any = {
+    fname: '',
+    lName: '',
+    userName: '',
+    city: ''
+  }
 }
