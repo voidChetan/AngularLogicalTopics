2d36eeddd8d7a014055b9e6c28c44797123ad7ff
codefirst2022 <codefirst2022@gmail.com>
2025-02-04 09:54:20 +0530

    changes

--- src/app/core/services/master.service.ts
+++ src/app/core/services/master.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, map, Observable, retry, shareReplay, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { myConstant } from '../constant';
 
@@ -10,16 +10,61 @@ import { myConstant } from '../constant';
 export class MasterService {
 
   private apiEndPoint: string = '';
+
+  userList$: Observable<any>;
+  ondataChnag2: BehaviorSubject<any>  = new BehaviorSubject<any>({});
+
   constructor(private http: HttpClient) {
     this.apiEndPoint = environment.apiEndPoint;
    }
 
   getAllRequest() : Observable<any[]> {
-    return this.http.get<any[]>(this.apiEndPoint +
-       myConstant.apiMethodName.allRequest);
+    return this.http.get<any[]>(this.apiEndPoint + myConstant.apiMethodName.allRequest);
   }
 
   createFullname(fname:string,mname:string,lname:string){
     return fname+ " "+ mname + " "+ lname;
+  }
+
+  getAllJsonUsers() {
+    if(!this.userList$){
+      this.userList$ =  this.http.get("https://jsonplaceholder.typicode.com/users").pipe(
+        shareReplay(1)
+       );
+    }
+    
+     return this.userList$;
+  }
+  getJsonUser() {
+    return this.http.get("https://jsonplaceholder.typicode.com/users");
+  }
+
+  getJsonUserById(id) {
+    return this.http.get("https://jsonplaceholder.typicode.com/users/"+id);
+  }
+
+  getUsers() {
+    return this.http.get("https://projectapi.gerasim.in/api/SoilTest/GetAllUsers").pipe(
+      map((result:any)=> result.data)
+    );
+  }
+
+  getUsersDatForDropdown() {
+    return this.http.get("https://projectapi.gerasim.in/api/SoilTest/GetAllUsers").pipe(
+      tap((res:any)=>{
+        
+        this.ondataChnag2.next(res);
+      }),
+      retry(3),
+      map((result:any)=> result.data),
+      map((res:any) => {
+        
+       return res.map((user:any)=> {
+          
+          return {userId: user.userId,fullName:user.fullName} 
+        }) 
+      }),
+     
+    );
   }
 }
--- src/app/pages/employee/employee.component.ts
+++ src/app/pages/employee/employee.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { MyTableComponent } from 'src/app/reusable/my-table/my-table.component';
 import { MasterService } from 'src/app/core/services/master.service';
+import { BehaviorSubject, concatMap, forkJoin, Observable, of, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-employee',
@@ -30,10 +31,63 @@ export class EmployeeComponent {
     },
   ];
   requests: any[]= [];
-  constructor(private master: MasterService) {}
+
+  onDatChnage: Subject<boolean> = new Subject<boolean>();
+
+  ondataChnag2: BehaviorSubject<boolean>  = new BehaviorSubject<boolean>(false);
+
+  data : Observable<boolean> = new Observable<boolean>();
+
+  CityList = of(["Pune","Nagpur"]);
+
+  userList: any []= [];
+  
+
+  constructor(private master: MasterService) {
+
+  }
   ngOnInit() {
-    this.loadUser();
-    this.loadAdminData();
+    this.master.getAllJsonUsers().subscribe((res:any)=>{
+      debugger;
+    })
+    setTimeout(() => {
+      this.getUserList()
+    }, 5000);
+    
+    setTimeout(() => {
+      this.getUserList()
+    }, 10000);
+    // const userData = this.master.getUsers();
+    // const userDropdownData = this.master.getUsersDatForDropdown();
+    // const allRequest = this.master.getAllRequest();
+    // debugger;
+    // forkJoin([userData,userDropdownData]).subscribe((res: any)=>{
+    //   debugger;
+    // })
+    // this.master.getJsonUser().pipe(
+    //   concatMap((res:any) => this.master.getJsonUserById(res[0].id)))
+    //   .subscribe((res:any)=>{
+    //     debugger;
+    //   }
+    // )
+
+    // this.loadUser();
+    // this.loadAdminData();
+    // this.getUserList();
+    // this.getUserDropdwoList()
+  }
+
+  getUserList() {
+    this.master.getAllJsonUsers().subscribe((res:any)=>{
+      
+      this.userList =  res;
+    })
+  }
+  getUserDropdwoList() {
+    this.master.getUsersDatForDropdown().subscribe((res:any)=>{
+      debugger;;
+      this.userList =  res;
+    })
   }
   loadAdminData() {
       
